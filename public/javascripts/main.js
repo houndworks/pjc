937a8ae81e54988475f2411908b279370c2acda7
Daniel Cartwright <dan@houndworld.org>
2014-05-28 23:14:49 +0100

    Style image thumbnails & get lightbox working.

---
+++ public/javascripts/main.js
@@ -0,0 +1,44 @@
+function equalizeHeights(items) {
+  var maxHeight = 0;
+  items.each(function() {
+    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+  });
+  items.each(function() { $(this).css('min-height',(maxHeight+10)+"px"); });
+}
+
+function formatPostListings() {
+  var posts = $('body.posts div.posts div.thumbnail');
+  posts.each(function(){ $(this).css('min-height',"0px"); });
+  if( $(window).width() >= 992 ) {
+    for(var i = 0; i < posts.length; i+=3) { equalizeHeights( posts.slice(i, i+3) ); }
+  } else if ( $(window).width() >= 768 ) {
+    for(var i = 0; i < posts.length; i+=2) { equalizeHeights( posts.slice(i, i+2) ); }
+  }
+}
+
+
+$(document).ready(function(){
+
+  // if they click on a boxed image, enlarge it
+  $('div.img-box a').click(function(event) {
+    event.preventDefault();
+    var box =
+      '<div id="lightbox">' +
+      '<p>Click to Close</p>' +
+      '<img src="' + $(this).attr('href') + '" alt="Enlarged" />' +
+      '<p class="caption">...Caption...</p>' +
+      '</div>';
+    $('body').append(box);
+  });
+
+  // if they click an enlarged image, close it
+  $('body').on('click', '#lightbox',function(event) {
+    $('#lightbox').remove();
+  });
+
+  $(window).on('load', formatPostListings);
+  $(window).on('resize', formatPostListings);
+
+
+});
+
