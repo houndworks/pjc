4cf65fec6db0099a9167d95d9116ccfe1d7ae639
Daniel Cartwright <dan@houndworld.org>
2014-05-30 14:43:19 +0100

    Remove old lightbox and add new Colorbox files.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -17,28 +17,3 @@ function formatPostListings() {
 }
 
 
-$(document).ready(function(){
-
-  // if they click on a boxed image, enlarge it
-  $('div.img-box a').click(function(event) {
-    event.preventDefault();
-    var box =
-      '<div id="lightbox">' +
-      '<p>Click to Close</p>' +
-      '<img src="' + $(this).attr('href') + '" alt="Enlarged" />' +
-      '<p class="caption">...Caption...</p>' +
-      '</div>';
-    $('body').append(box);
-  });
-
-  // if they click an enlarged image, close it
-  $('body').on('click', '#lightbox',function(event) {
-    $('#lightbox').remove();
-  });
-
-  $(window).on('load', formatPostListings);
-  $(window).on('resize', formatPostListings);
-
-
-});
-
